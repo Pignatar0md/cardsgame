b16656f171e300c91c93fb096b6bdb22e351326e
Marcelo Pignataro <pignataromd@gmail.com>
2026-09-10 17:01:13 +0100

    Initial Commit

---
+++ cardsgame/cardsgameApp.swift
@@ -0,0 +1,17 @@
+//
+//  cardsgameApp.swift
+//  cardsgame
+//
+//  Created by Marcelo Pignataro on 10/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct cardsgameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ cardsgameTests/cardsgameTests.swift
@@ -0,0 +1,16 @@
+//
+//  cardsgameTests.swift
+//  cardsgameTests
+//
+//  Created by Marcelo Pignataro on 10/09/2026.
+//
+
+import Testing
+
+struct cardsgameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
