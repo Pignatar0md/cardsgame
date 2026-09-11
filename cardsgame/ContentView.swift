a387b7f7c985b7ce9bc013277e265d80ac0834ee
Marcelo Pignataro <pignataromd@gmail.com>
2026-09-11 11:29:41 +0100

    initial commit

--- cardsgame/ContentView.swift
+++ cardsgame/ContentView.swift
@@ -8,14 +8,73 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var playerCard = "card13";
+    @State var machineCard = "card10";
+    @State var playerScore = 0;
+    @State var machineScore = 0;
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Image("background-plain")
+            VStack {
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack {
+                    Spacer()
+                    Image(playerCard)
+                    Spacer()
+                    Image(machineCard)
+                    Spacer()
+                }
+                Spacer()
+                Button {
+                    dealCards()
+                } label: {
+                    Image("button")
+                }
+                Spacer()
+                HStack {
+                    Spacer()
+                    VStack {
+                        Text("PLAYER")
+                            .font(.headline)
+                            .padding(.bottom)
+                        Text(String(playerScore))
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                    VStack {
+                        Text("MACHINE")
+                            .font(.headline)
+                            .padding(.bottom)
+                        Text(String(machineScore))
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                }
+                .foregroundStyle(.white)
+                Spacer()
+                Spacer()
+            }
+            .padding()
         }
-        .padding()
+    }
+    
+    func dealCards() {
+        var cardStr = "card";
+        var playerCardNumber = Int.random(in: 2...14)
+        playerCard = cardStr + String(playerCardNumber);
+        
+        var machineCardNumber = Int.random(in: 2...14)
+        machineCard = cardStr + String(machineCardNumber);
+        
+        if playerCardNumber > machineCardNumber {
+            playerScore += 1
+        } else if playerCardNumber < machineCardNumber {
+            machineScore += 1
+        }
     }
 }
 
